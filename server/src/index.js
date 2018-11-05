a8ef3ec7365e28c9b5e9b9f4c333551a42c0f087
Jim <linchenglong12@gmail.com>
2018-11-05 01:02:39 -0600

    test

--- server/src/index.js
+++ server/src/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const { exec } = require('child_process');
+const path = require('path');
 const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.json());
 
 //Serve static files
-app.use('/static', express.static('./client/public'));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 //Query api
 app.post('/api/query', (req, res) => {
@@ -22,7 +23,7 @@ app.post('/api/query', (req, res) => {
 
 //The "catchall" handler: for any request that doesn't routes above
 app.get('*', (req, res) => {
-	res.status(404).send("404! Page not found!")
+	res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
 const port = process.env.PORT || 5000;
