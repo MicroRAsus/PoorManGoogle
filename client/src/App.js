283807b8b05cde296cf09024c185089e2427ed6b
Jim <linchenglong12@gmail.com>
2018-11-05 03:21:49 -0600

    Front end mostly done. TODO: Result list and result entry components

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,58 @@
 import React, { Component } from 'react';
+import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+	constructor(props) {
+	    super(props);
+
+	    this.state = {
+	      q: "",
+		  r:[]
+	    };
+  	}
+
+	validateForm() {
+    	return this.state.q.length > 0;
+  	}
+
+	handleChange = event => {
+		this.setState({ q: event.target.value });
+  	}
+
+	handleSubmit = event => {
+    	event.preventDefault();
+  	}
+
+	render() {
+		return (
+			<div className="App">
+				<div className="App-bar">
+					<img src={logo} className="App-logo" alt="logo" />
+					<form onSubmit={this.handleSubmit}>
+						<FormGroup controlId="query" bsSize="large">
+							<FormControl
+								autoFocus
+		            			type="text"
+								value={this.state.q}
+		            			placeholder="Enter your query here..."
+								onChange={this.handleChange}
+		          			/>
+						</FormGroup>
+						<Button
+		            		block
+		            		bsSize="medium"
+		            		type="submit"
+							disabled={!this.validateForm()}
+		          		>
+							Nuke!
+						</Button>
+					</form>
+				</div>
+			</div>
+    	);
+	}
 }
 
 export default App;
