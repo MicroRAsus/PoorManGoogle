c1fafc5a7cfec856a5415fd1613a2d99e738dec2
Jim <linchenglong12@gmail.com>
2018-11-04 22:57:44 -0600

    more test

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,20 +1,18 @@
 const express = require('express');
-//const path = require('path');
 const { exec } = require('child_process');
 const bodyParser = require('body-parser');
 
 const app = express();
 app.use(bodyParser.json());
 
-// Serve static files from the React app
-app.use(express.static('./Client/files'));
+//Serve static files
+app.use('/static', express.static('./Client/public'));
 
-// Put all API endpoints under '/api'
+//Query api
 app.post('/api/query', (req, res) => {
 	res.setHeader('Content-Type', 'application/json');
-	exec(`./Server/bin/query '${req.body.q}'`, (err, stdout, stderr) => {
-		if (err) {
-    		// node couldn't execute the command
+	exec(`./Server/bin/query '${req.body.q}'`, (err, stdout, stderr) => { //no need to be sanitized as it's wrapped around single quotes
+		if (err) {//error occured executing query binary
     		res.status(500).send({ });
 			return;
   		}
@@ -22,13 +20,13 @@ app.post('/api/query', (req, res) => {
 	});
 });
 
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-// app.get('*', (req, res) => {
-// 	res.sendFile(path.parse('/Client/files'));
-// });
+//The "catchall" handler: for any request that doesn't
+//match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+	res.status(404).send("404! Page not found!")
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port);
 
-console.log(`Express listening on ${port}`);
+console.log(`Poor mans Google search engine (Express) is listening on port ${port}`);
