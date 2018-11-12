634b183e5cd53234deb6fc364587d516bc6a097b
Jim <linchenglong12@gmail.com>
2018-11-11 20:30:46 -0600

    Working front end

--- client/src/container/Result.js
+++ client/src/container/Result.js
@@ -1,10 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 const Result = (props) => {
 	return (
 		<div>
-			 <Link to={"/files/" + props.url}>{props.url}</Link>
+			 <a href={"/files/" + props.url}>{props.url}</a>
 		</div>
 	);
 }
